ab12ca0cd1850b2a4c9b6b4c1d19a1f67a88a746
Sluchaev Kirill <k.sluchaev@yandex.ru>
2025-03-07 21:27:49 +0300

    Add xl2tp config filler

--- internal/service/adapters/linux/config_filler.go
+++ internal/service/adapters/linux/config_filler.go
@@ -1,13 +1,21 @@
 package linux
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
-	"github.com/katalix/go-l2tp/config"
+	"implementation/internal/domain/config"
+	"log"
+	"os"
+	"path"
+	"strings"
 )
 
 const (
 	baseTemplatesPath = "config/templates"
+	xl2tpPath         = "/etc/xl2tpd/xl2tpd.conf"
+	optionsPath       = "/etc/ppp/options.l2tp"
+	chapSecretsPath   = "/etc/ppp/chap-secrets"
 )
 
 type FileFiller func(config *config.Config) error
@@ -32,9 +40,9 @@ func (filler *ConfigFiller) FillConfig(userConfig *config.Config) error {
 	}
 
 	fileFillers := map[string]FileFiller{
-		"xl2tpd.conf":  FillXL2TP_conf,
-		"options.l2tp": FillOPTIONS_l2tp,
-		"chap-secrets": FillCHAP_SECRETS,
+		"xl2tpd.conf":  filler.fillXL2TP_conf,
+		"options.l2tp": filler.fillOPTIONS_l2tp,
+		"chap-secrets": filler.fillCHAP_SECRETS,
 	}
 
 	for fileName, fileFiller := range fileFillers {
@@ -46,14 +54,123 @@ func (filler *ConfigFiller) FillConfig(userConfig *config.Config) error {
 	return nil
 }
 
-func FillXL2TP_conf(config *config.Config) error {
+func (filler *ConfigFiller) fillXL2TP_conf(userConfig *config.Config) error {
+	var (
+		file *os.File
+		err  error
+	)
+
+	if !isFileExists(xl2tpPath) {
+		log.Printf("xl2tp config file %s does not exist", xl2tpPath)
+
+		if err := copyFile(path.Join(filler.templatesPath, "xl2tp_global_template.ini"), xl2tpPath); err != nil {
+			return fmt.Errorf("failed to copy xl2tp_global_template.ini: %w", err)
+		}
+	}
+	log.Printf("filling file %s", xl2tpPath)
+
+	file, err = os.OpenFile(xl2tpPath, os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("failed to open xl2tp.config file: %v", err)
+	}
+
+	defer file.Close()
+
+	for _, server := range userConfig.Servers {
+		for _, user := range server.Users {
+			log.Printf("filling user [%v]", user.Username)
+
+			contains, err := isContainsInFile(file, fmt.Sprintf("[lac %s]", user.Username))
+			if err != nil {
+				return fmt.Errorf("failed to check if contains %s: %w", user.Username, err)
+			}
+
+			if !contains {
+				if err := filler.insertLNSSection(file, user.Username, server.Address); err != nil {
+					return fmt.Errorf("failed to insert lns section: %w", err)
+				}
+			}
+		}
+	}
+
 	return nil
 }
 
-func FillOPTIONS_l2tp(config *config.Config) error {
+func (filler *ConfigFiller) insertLNSSection(file *os.File, userName, serverAddress string) error {
+	templateFile, err := os.Open(path.Join(filler.templatesPath, "xl2tp_lns_template.ini"))
+	if err != nil {
+		return fmt.Errorf("failed to open xl2tp_lns_template.ini: %w", err)
+	}
+
+	scanner := bufio.NewScanner(templateFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		line = strings.Replace(line, "%username%", userName, 1)
+		line = strings.Replace(line, "%server_ip%", serverAddress, 1) + "\n"
+
+		if err := appendToFile(file, line); err != nil {
+			return err
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return templateFile.Close()
+}
+
+func (filler *ConfigFiller) fillOPTIONS_l2tp(config *config.Config) error {
 	return nil
 }
 
-func FillCHAP_SECRETS(config *config.Config) error {
+func (filler *ConfigFiller) fillCHAP_SECRETS(config *config.Config) error {
+	return nil
+}
+
+func isFileExists(path string) bool {
+	if _, err := os.Stat(path); err == nil {
+		return true
+	}
+
+	return false
+}
+
+func appendToFile(file *os.File, data string) error {
+	_, err := file.WriteString(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func isContainsInFile(file *os.File, substring string) (bool, error) {
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, substring) {
+			return true, nil
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return false, err
+	}
+
+	return false, nil
+}
+
+func copyFile(src, destination string) (err error) {
+	data, err := os.ReadFile(src)
+	if err != nil {
+		log.Fatalf("failed to read src: %v", err)
+	}
+
+	if err := os.WriteFile(destination, data, 0644); err != nil {
+		log.Fatalf("failed to write to target: %v", err)
+	}
+
 	return nil
 }
