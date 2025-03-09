654ce3b79d3046398b8bb3bf3858fd139ad9f458
Sluchaev Kirill <k.sluchaev@yandex.ru>
2025-03-09 11:33:31 +0300

    Add config repository implementation

--- go.mod
+++ go.mod
@@ -7,18 +7,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
 	golang.org/x/net v0.35.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
-require (
-	github.com/go-kit/kit v0.13.0 // indirect
-	github.com/go-kit/log v0.2.1 // indirect
-	github.com/go-logfmt/logfmt v0.6.0 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/josharian/native v1.1.0 // indirect
-	github.com/katalix/go-l2tp v0.1.8 // indirect
-	github.com/mdlayher/genetlink v1.3.2 // indirect
-	github.com/mdlayher/netlink v1.7.2 // indirect
-	github.com/mdlayher/socket v0.4.1 // indirect
-	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-)
+require golang.org/x/sys v0.30.0 // indirect
--- internal/repository/config_repository.go
+++ internal/repository/config_repository.go
@@ -1,3 +1,44 @@
 package repository
 
-//TODO implement read config from yaml file
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
+
+	"gopkg.in/yaml.v3"
+	"implementation/internal/domain/config"
+)
+
+type FileRepository struct {
+	path string
+}
+
+func NewFileRepository(path string) *FileRepository {
+	return &FileRepository{
+		path: path,
+	}
+}
+
+func (r *FileRepository) GetConfig(name string) (*config.Config, error) {
+	file, err := os.Open(path.Join(r.path, name))
+	if err != nil {
+		return nil, fmt.Errorf("failed to open config file: %w", err)
+	}
+
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read data from config file: %w", err)
+	}
+
+	connectionConfig := config.Config{}
+
+	if err := yaml.Unmarshal(data, &connectionConfig); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	return &connectionConfig, nil
+}
