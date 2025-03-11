668880805d353664b2ecaa60b828d75754c46c61
Sluchaev Kirill <k.sluchaev@yandex.ru>
2025-03-11 21:33:38 +0300

    Add connection cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"implementation/internal/cli"
 	"implementation/internal/controller"
 	"implementation/internal/service"
 	"implementation/internal/service/adapters/linux"
 	"log"
-	"os"
 )
 
 func main() {
@@ -13,9 +13,42 @@ func main() {
 	connectService := service.NewConnectionService(adapter)
 	ctrl := controller.NewConnectionController(connectService)
 
-	args := os.Args
+	//err := ctrl.TunnelConnect("kirill")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
-	err := ctrl.Proceed(args[1:])
+	//repo := repository.NewFileRepository("config")
+	//config, err := repo.GetConfig("config-example.yaml")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//log.Println(config)
+
+	//filler := linux.NewConfigFiller("config/templates")
+	//
+	//err := filler.FillConfig(&config.Config{Servers: []config.ServerConfig{
+	//	{
+	//		Address: "45.45.45.45",
+	//		Users: []config.UserConfig{
+	//			{
+	//				Username: "admin",
+	//				Password: "qwerty007",
+	//			},
+	//			{
+	//				Username: "admin1",
+	//				Password: "qwerty00723232",
+	//			},
+	//		},
+	//	},
+	//}})
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	clientCLI := cli.New(ctrl)
+	err := clientCLI.Execute()
 	if err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -6,8 +6,13 @@ require (
 	github.com/google/gopacket v1.1.19
 	github.com/joho/godotenv v1.5.1
 	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
+	github.com/spf13/cobra v1.9.1
 	golang.org/x/net v0.35.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
-require golang.org/x/sys v0.30.0 // indirect
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+)
