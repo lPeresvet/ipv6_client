6e7438bf6cbdbeab017a663b8a6e58bea7ee9b97
Sluchaev Kirill <k.sluchaev@yandex.ru>
2025-04-06 11:03:39 +0300

    Add config watcher folder

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #cleanup previous build
 rm -f ./client
+rm -f ./watcher
 
 #build client app
 go build -o ./client ./client_src/cmd/main.go
+
+#build watcher app
+go build -o ./watcher ./connection_watcher/cmd/main.go
